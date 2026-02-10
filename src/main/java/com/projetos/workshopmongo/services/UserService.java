6b9af4dc2b99cee277f90fe137823c2f9da36fb9
Vinicius <vinicius@lambdatt.com.br>
2026-02-10 00:56:05 -0300

    GetById

--- src/main/java/com/projetos/workshopmongo/services/UserService.java
+++ src/main/java/com/projetos/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.projetos.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.projetos.workshopmongo.domain.User;
 import com.projetos.workshopmongo.repository.UserRepository;
+import com.projetos.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,6 +19,12 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		// Estudar sobre o Optional e sobre o orElseThrow
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
 	}
 
 }
