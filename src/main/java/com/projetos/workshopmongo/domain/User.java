c8840b85c8de53104e83f9dd8526a5dda11cf358
Vinicius <vinicius@lambdatt.com.br>
2026-02-10 11:45:18 -0300

    Post reference

--- src/main/java/com/projetos/workshopmongo/domain/User.java
+++ src/main/java/com/projetos/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package com.projetos.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document // Mapea a coleção com o mesmo nome da Classe
@@ -16,6 +19,9 @@ public class User implements Serializable{
 	private String id;
 	private String name;
 	private String email;
+	
+	@DBRef(lazy = true) // Não carregar automaticamente caso não for acessado diretamente
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {}
 
@@ -48,6 +54,16 @@ public class User implements Serializable{
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	// Métodos para permitir as comparações entre objetos
