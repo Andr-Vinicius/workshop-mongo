b8a5ec2ae712bed2256cc3723f6901754e77bd08
Vinicius <vinicius@lambdatt.com.br>
2026-02-10 11:49:57 -0300

    FindPosts

--- src/main/java/com/projetos/workshopmongo/resources/UserResource.java
+++ src/main/java/com/projetos/workshopmongo/resources/UserResource.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.projetos.workshopmongo.domain.Post;
 import com.projetos.workshopmongo.domain.User;
 import com.projetos.workshopmongo.dto.UserDTO;
 import com.projetos.workshopmongo.services.UserService;
@@ -66,5 +67,11 @@ public class UserResource {
 		obj = service.update(obj);
 			
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
