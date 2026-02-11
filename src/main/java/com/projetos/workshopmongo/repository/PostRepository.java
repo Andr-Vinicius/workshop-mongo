eeca53a4e2facce8b1b85a34cc9a81dd25c9c96b
Vinicius <vinicius@lambdatt.com.br>
2026-02-10 23:58:56 -0300

    Query methods

--- src/main/java/com/projetos/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/projetos/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.projetos.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.projetos.workshopmongo.domain.Post;
@@ -11,5 +12,8 @@ import com.projetos.workshopmongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String>{
 	
 	List<Post> findByTitleContainingIgnoreCase(String text); // Query Methods
+	
+	@Query("{'title': {$regex: ?0, $options: 'i'}}")
+	List<Post> searchTitle(String text);
 	
 }
